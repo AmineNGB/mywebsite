644168f14a11edc9b463c90b102fc7cb548d5d1e
Amine <amineneghbel@gmail.com>
2020-07-01 17:35:09 +0200

    Updates

---
+++ precache-manifest.a581aee7e5946706b7bce55a42583c3d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7821172c1788c945549deafd8a2a44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abe3d2b56c8463d08d8a",
+    "url": "/static/css/main.edbe2895.chunk.css"
+  },
+  {
+    "revision": "08931cffbf0e4dd0a389",
+    "url": "/static/js/2.69aa3371.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.69aa3371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe3d2b56c8463d08d8a",
+    "url": "/static/js/main.f37de775.chunk.js"
+  },
+  {
+    "revision": "8a6f7fe37015634ac72b",
+    "url": "/static/js/runtime-main.7aaf2284.js"
+  },
+  {
+    "revision": "1c8cb44ae5fb8a0d09d048e566171bc3",
+    "url": "/static/media/background-amine.1c8cb44a.jpg"
+  },
+  {
+    "revision": "7741b0076583695c7fe6f64edacb0339",
+    "url": "/static/media/planet.7741b007.jpg"
+  },
+  {
+    "revision": "75b2e518a1c87c2a039fd2c1c93cc9e1",
+    "url": "/static/media/shape.75b2e518.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mywebsite/precache-manifest.63712c4688cbd78ffd9a7ba3b78a9edc.js"
+  "/precache-manifest.a581aee7e5946706b7bce55a42583c3d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mywebsite/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
